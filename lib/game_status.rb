f398f9d401ddefb44146819cadee0c26233d7590
Veniel Dorestave <veniel@noukod.com>
2018-09-03 19:32:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,51 +23,29 @@ def display_board(board)
 end
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combo|
-    #check for player 1 win
-    if check_win_combination?(board, 'X', win_combo)
-      return win_combo
-      #check for player 2 win
-    elsif check_win_combination?(board, 'O', win_combo)
-      return win_combo
-    else
-      return false
-    end
+  WIN_COMBINATIONS.find do |combo|
+    # original argument "position_taken?(board, combo)"
+    position_taken?(board, combo[0]) &&
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[0]] == board[combo[2]]
   end
 end
-
 def full?(board)
-  if board.include?(' ') || board.include?('')
-    return false
-else
-    return true
+  board.all? do |index|
+    index != " "
   end
 end
 
 def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  end
+  full?(board)
+  !(won?(board))
 end
 def over?(board)
-  puts 'is it over?'
-  if won?(board) || draw?(board) || full?(board)
-    return true
-  else
-    puts 'no keep going'
-    return false
-  end
+  draw?(board) || won?(board)
+  full?(board)
 end
-
 def winner(board)
-  if !won?(board)
-    return nil
-  else WIN_COMBINATIONS.each do |win_combo|
-    if check_win_combination?(board, 'X', win_combo)
-      return 'X'
-    elsif check_win_combination?(board, 'O', win_combo)
-      return 'O'
-    end
+  if won?(board)
+   return board[won?(board)[0]]
   end
-end
 end
