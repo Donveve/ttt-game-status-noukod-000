757cb61ac5d2f2631d033ae122c2340d819340ab
Veniel Dorestave <veniel@noukod.com>
2018-09-03 19:21:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,6 +13,7 @@ WIN_COMBINATIONS = [
   [2,5,8],
   [0,4,8],
   [2,4,6]]
+  
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -21,28 +22,54 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def won?(board)
-  WIN_COMBINATIONS.find do |combo|
-    # original argument "position_taken?(board, combo)"
-    position_taken?(board, combo[0]) &&
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[0]] == board[combo[2]]
+  WIN_COMBINATIONS.each do |win_combo|
+    #check for player 1 win
+    if check_win_combination?(board, 'X', win_combo)
+      return win_combo
+      #check for player 2 win
+    elsif check_win_combination?(board, 'O', win_combo)
+      return win_combo
+    else
+      return false
+    end
   end
 end
 def full?(board)
-  board.all? do |index|
-    index != " "
+  if board.include?(' ') || board.include?('')
+    return false
+else
+    return true
   end
 end
+
+#IS THERE A DRAW?
 def draw?(board)
-  full?(board)
-  !(won?(board))
-end
-def over?(board)
-  won?(board)
-  !full?(board)
-end
-def winner(board)
-  if won?(board)
-   return board[won?(board)[0]]
+  if !won?(board) && full?(board)
+    return true
   end
+end
+
+#HAS SOMETHING HAPPENED?
+def over?(board)
+  puts 'is it over?'
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    puts 'no keep going'
+    return false
+  end
+end
+
+#WHO WON?
+def winner(board)
+  if !won?(board)
+    return nil
+  else WIN_COMBINATIONS.each do |win_combo|
+    if check_win_combination?(board, 'X', win_combo)
+      return 'X'
+    elsif check_win_combination?(board, 'O', win_combo)
+      return 'O'
+    end
+  end
+end
 end
