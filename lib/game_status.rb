76299929c923d4916091b7ede73359da4f811a5f
Veniel Dorestave <veniel@noukod.com>
2018-09-03 19:29:55 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,6 +21,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
     #check for player 1 win
@@ -34,6 +35,7 @@ def won?(board)
     end
   end
 end
+
 def full?(board)
   if board.include?(' ') || board.include?('')
     return false
@@ -42,14 +44,11 @@ else
   end
 end
 
-#IS THERE A DRAW?
 def draw?(board)
   if !won?(board) && full?(board)
     return true
   end
 end
-
-#HAS SOMETHING HAPPENED?
 def over?(board)
   puts 'is it over?'
   if won?(board) || draw?(board) || full?(board)
@@ -60,7 +59,6 @@ def over?(board)
   end
 end
 
-#WHO WON?
 def winner(board)
   if !won?(board)
     return nil
