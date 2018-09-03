55591571717e63f7cec38ef6a9634d82020f6cd7
Veniel Dorestave <veniel@noukod.com>
2018-09-03 19:14:05 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,16 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0,1,2], #Top row
-  [3,4,5], #Middle row
-  [6,7,8], #Bottom row
-  [0,3,6], #Left column
-  [1,4,7], #Middle column
-  [2,5,8], #Right column
-  [0,4,8], #Left start diagonal
-  [2,4,6]  #Right start diagonal
-]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -39,8 +30,8 @@ def draw?(board)
   !(won?(board))
 end
 def over?(board)
-  draw?(board) || won?(board)
-  full?(board)
+  won?(board)
+  !full?(board)
 end
 def winner(board)
   if won?(board)
